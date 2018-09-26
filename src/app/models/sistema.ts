bd8fc3e3afe0953955dcad542cdcaa101ca3209b
sophia <sophiavigoya@gmail.com>
2018-09-26 14:37:54 -0500

    se modifica modelo Sistema

--- src/app/models/sistema.ts
+++ src/app/models/sistema.ts
@@ -1,3 +1,8 @@
+/**
+ * Modelo que representa un Sistema de construcción de la aplicación. 
+ * @author Sophia
+ * @version 1.0
+ */
 export interface Sistema{
   key?: string;
   descripcion: string;
