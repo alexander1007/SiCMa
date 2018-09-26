0f9152f46ec482c832e1e9b1e78fc4e487599c57
alexander <alexander.satizabal1007@gmail.com>
2018-09-26 13:13:52 -0500

    Redondeo de valores

--- src/pages/resultado-calculo/resultado-calculo.ts
+++ src/pages/resultado-calculo/resultado-calculo.ts
@@ -31,9 +31,9 @@ export class ResultadoCalculoPage {
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
-      minimumFractionDigits: 2
+      minimumFractionDigits: 0
     });
-    this.valorTotalM= formatter.format( parseFloat(this.valorTotal)) // "$1,000.00"
+    this.valorTotalM= formatter.format( parseFloat(this.valorTotal)) // "$1,000"
 
   }
    //esto es para desactivar los menu en la pantalla login
