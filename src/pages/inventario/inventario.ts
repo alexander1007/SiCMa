9b39c05f25aa0f3d27620780423cb017dd5608b7
yhyenifer <yhyenifer@gmail.com>
2019-11-25 18:00:42 -0500

    cotizaciones pdf

--- src/pages/inventario/inventario.ts
+++ src/pages/inventario/inventario.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, Platform, LoadingController } from 'ionic-angular';
 import { PedidoPage } from '../pedido/pedido';
 import { ListaInventariosService } from '../../services/inventario/inventario.service';
 import * as moment from 'moment';
 
-/**
- * Generated class for the InventarioPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+// pdf
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { FileOpener } from '@ionic-native/file-opener';
+import { File } from '@ionic-native/file';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 @IonicPage()
 @Component({
@@ -18,6 +19,8 @@ import * as moment from 'moment';
 })
 export class InventarioPage {
 
+  valorTotal: any = 0;
+  pdfObjet: any;
   identificacion: any;
   cliente: any;
   productosAgregados: any[];
@@ -34,13 +37,18 @@ export class InventarioPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private alertCtrl: AlertController,
+    public platform: Platform,
+    public file: File,
+    public fileOpener: FileOpener,
+    public loadingCtrl: LoadingController,
     public inventariosService: ListaInventariosService) {
 
     this.productosAgregados = [];
+    this.fecha = moment().format('YYYY-MM-DD');
+    // console.log(Number(1500).toLocaleString('en'));
 
     this.inventariosService.getListaInventarios().valueChanges()
       .subscribe(data => {
-        console.log(data);
         this.productos = data;
         this.productos.sort(function (a, b) {
           return a.nombre.localeCompare(b.nombre);
@@ -93,7 +101,7 @@ export class InventarioPage {
     })
   }
   terminarPedido() {
-    if ((this.cliente == '' || this.cliente == undefined) && (this.identificacion == '' || this.identificacion == undefined)) {
+    if ((this.cliente == '' || this.cliente == undefined) || (this.identificacion == '' || this.identificacion == undefined)) {
       this.agregarCliente(true);
     } else {
       this.crearPdf(this.cliente, this.identificacion, this.productosAgregados);
@@ -110,7 +118,6 @@ export class InventarioPage {
   }
 
   filtrar() {
-    console.log('filtrar', this.filtro);
     this.initializeItems();
     if (this.filtro) {
       this.productos$ = this.productos.filter((item) => {
@@ -140,18 +147,30 @@ export class InventarioPage {
       ],
       buttons: [
         {
+          text: 'Cancelar'
+        },
+        {
           text: 'Aceptar',
           handler: data => {
             this.cliente = data.cliente;
             this.identificacion = data.identificacion;
             if (final) {
-              this.crearPdf(this.cliente, this.identificacion, this.productosAgregados);
+              if (this.cliente != '' && this.cliente != undefined && this.identificacion != '' && this.identificacion != undefined) {
+                this.crearPdf(this.cliente, this.identificacion, this.productosAgregados);
+
+              } else {
+                const alert = this.alertCtrl.create({
+                  title: 'PlaCMa',
+                  subTitle: 'El nombre del cliente y la identificación/NIT es requerida. Ingrese un valor.',
+                  buttons: ['OK']
+                });
+                alert.present();
+                return;
+              }
             }
           }
-        },
-        {
-          text: 'Cancelar'
         }
+
       ]
     });
     alert.present();
@@ -160,6 +179,161 @@ export class InventarioPage {
 
 
   crearPdf(cliente, identificacion, productosAgregados) {
-    console.log('crear pdf', cliente, identificacion, productosAgregados);
+    // preparar productos
+    let info = new Array();
+    let titulo = [
+      { text: 'Producto', style: 'tableHeader' },
+      { text: 'Descripción', style: 'tableHeader' },
+      { text: 'Valor U.', style: 'tableHeader' },
+      { text: 'Cantidad', style: 'tableHeader' },
+      { text: 'Valor Total', style: 'tableHeader' }
+
+    ];
+    info.push(titulo);
+    for (let i = 0; i < this.productosAgregados.length; i++) {
+      this.valorTotal = this.valorTotal + (this.productosAgregados[i]['valor'] * this.productosAgregados[i]['cantidad']);
+      info.push([this.productosAgregados[i]['nombre'], this.productosAgregados[i]['descripcion'], '$ ' + Number(this.productosAgregados[i]['valor']).toLocaleString('en'),
+      this.productosAgregados[i]['cantidad'], '$ ' + Number(this.productosAgregados[i]['valor'] * this.productosAgregados[i]['cantidad']).toLocaleString('en')]);
+    }
+
+    let footerTable = [
+      { text: '', style: 'tableHeader' },
+      { text: '', style: 'tableHeader' },
+      { text: '', style: 'tableHeader' },
+      { text: 'Total', style: 'total' },
+      { text: '$ ' + Number(this.valorTotal).toLocaleString('en'), style: 'total' }
+    ]
+    info.push(footerTable);
+
+    let pdfDefinition = {
+      footer: {
+        stack: [
+          {
+            text: 'Cotización generada por PlaCMa',
+            alignment: 'center',
+            bold: true
+          }
+        ]
+      },
+      content: [
+        {
+          text: 'Cotización\n\n',
+          style: 'header',
+          alignment: 'center'
+        },
+        {
+          alignment: 'right',
+          columns: [
+            {
+              text: ''
+            },
+            {
+              text: 'Fecha: ' + this.fecha + '\n\n'
+            }
+          ]
+        },
+        {
+          columns: [
+            {
+              text: 'NIT/Identificación:',
+              alignment: 'left'
+            },
+            {
+              text: 'Cliente:',
+              alignment: 'right'
+            }
+          ], style: 'subheader',
+          fontSize: 15
+        },
+        {
+          columns: [
+            {
+              text: identificacion,
+              alignment: 'left'
+            },
+            {
+              text: cliente,
+              alignment: 'right'
+            }
+          ]
+        },
+
+        {
+          text: '\nProductos:\n',
+          style: 'subheader'
+        },
+        {
+          style: 'tableExample',
+          table: {
+            widths: [100, 100, 100, 50, 100],
+            body: info
+
+          }
+        },
+      ],
+      styles: {
+        header: {
+          fontSize: 20,
+          bold: true
+        },
+        subheader: {
+          fontSize: 15,
+          bold: true,
+          alignment: 'justify'
+        },
+        total: {
+          fontSize: 12,
+          bold: true,
+          alignment: 'justify'
+        },
+        tableExample: {
+          margin: [5, 5],
+          alignment: 'center'
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 12,
+          color: 'black'
+        }
+      }
+    };
+    let nombrePdf = this.fecha + "-cotizacion-" + cliente + ".pdf";
+    // cosntruccion del pdf y descarga
+    this.pdfObjet = pdfMake.createPdf(pdfDefinition);
+    this.openFile(nombrePdf);
   }
+
+
+  openFile(nombrePdf) {
+    this.presentLoading();
+    console.log(this.platform.is('cordova'));
+
+    if (this.platform.is('cordova')) {
+
+      this.platform.ready().then(() => {
+        this.pdfObjet.getBuffer((buffer) => {
+          var blob = new Blob([buffer], { type: 'application/pdf' });
+          this.file.writeFile(this.file.dataDirectory, nombrePdf, blob, { replace: true })
+            .then((fileEntry) => {
+              this.fileOpener.open(this.file.dataDirectory + nombrePdf, 'application/pdf');
+            });
+        });
+        return true;
+      });
+    } else {
+
+      this.pdfObjet.download();
+    }
+
+  }
+
+
+  presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: "Por favor espere...",
+      duration: 3000
+    });
+    loader.present();
+  }
+
 }
